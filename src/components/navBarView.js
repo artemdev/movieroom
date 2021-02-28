42987bd0c3c02f89885481ab620aae47fdc67c4c
Artem Zymovets <artwayprojects@gmail.com>
2021-03-01 01:10:10 +0200

    links addded

--- src/components/navBarView.js
+++ src/components/navBarView.js
@@ -5,7 +5,7 @@ export default function NavBar(props) {
     return (
         <header className={styles.navBar}>
             <div>
-                <img src="" alt="" /> movieroom.com
+                <img src="" alt="" /> movieroom.com <a href="/open">open</a>  <a href="/closed">closed</a>
             </div>
             {signIn ? <div><a href="#">login</a> <a href="#">logout</a></div> : <div>username <a href="#">logout</a></div>}
 
