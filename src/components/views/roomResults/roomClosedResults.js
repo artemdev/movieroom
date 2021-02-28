cfc38258e8025ae9f484dd5ed338c998e53ca65e
Artem Zymovets <artwayprojects@gmail.com>
2021-03-01 01:05:26 +0200

    room closed mockup

--- src/components/views/roomResults/roomClosedResults.js
+++ src/components/views/roomResults/roomClosedResults.js
@@ -1,6 +1,20 @@
-export default function RoomOpenResults(props) {
+import styles from './styles.module.css'
+
+export default function RoomClosedResults(props) {
     return (
-        <div>Hey it works!!</div>
+        <div>
+            <div className={styles.winnerCongratulations}>Congratulations!</div>
+
+            <h1 className={styles.winnerTitle}>Movie title</h1>
+            <div className={styles.winnerWrap}>
+                68%
+
+            </div>
+            <div className={styles.winnerVotes}>
+                2 votes
+            </div>
+
+        </div>
     )
 }
 
