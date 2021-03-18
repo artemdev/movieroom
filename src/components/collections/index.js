28381abc786884ee8140af2fc20a11a320933874
Artem Zymovets <artwayprojects@gmail.com>
2021-03-18 22:20:10 +0200

    modal

--- src/components/collections/index.js
+++ src/components/collections/index.js
@@ -1,93 +1,42 @@
 import styles from './styles.module.css'
 import Modal from './modal'
 import React, { useState } from 'react'
+import Collection from './collection'
 
 export default function Collections() {
+    const initialCollections = [
+       { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4"},{title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4"},{title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4"},{title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4"},{title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4"}]},
+        { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{ title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4" }] },
+        { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{ title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4" }] },
+        { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{ title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4" }] },
+        { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{ title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4" }] },
+        { id: 1, title: "One", url: "https://www.fillmurray.com/640/360", movies: [{ title: "one", year: "2020", genres: ["hello", "world"], coverUrl: "https://www.fillmurray.com/640/360", rating: "6.4", duration: "6.4" }] },
+       ]
     const [show, setShow] = useState(false)
-    const [collections, setCollections] = useState([
-        { id: 1, title: "hello" },
-         {id: 2, title: "world"}
-    ])
+    const [collections, setCollections] = useState(initialCollections)
 
-   const artem = [
-        { id: 1, title: "Artem" },
-         {id: 2, title: "Hello"}
-    ]
+ const [currentCollection, setCurrentCollection] = useState(initialCollections[0])
 
-    const openModalWithCollection = (collections=false) => {
+
+    const openModalWithCollection = (collection=false) => {
         setShow(true)
-        if (collections) {
-            setCollections(collections)
+        if (collection) {
+            setCurrentCollection(collection)
         }
     }
-
+    console.log(collections)
     return (
         <>
         <div className={styles.collections}>
             <h2 className={styles.collectionTitle}>Выберите коллекцию фильмов для голосования</h2>
                 <ul className={styles.collectionsList}>
-                <li onClick={()=> openModalWithCollection(artem)} className={styles.collectionItem}>
-                    <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                    <p className={styles.collectionText}>
-                        Романтические комедии
-                    </p>
-                </li>
-                <li className={styles.collectionItem}>   
-                    <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                    <p className={styles.collectionText}>
-                        Романтические комедии
-                    </p>
-                </li>
-                <li className={styles.collectionItem}>
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                <li className={styles.collectionItem}>
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                                <li className={styles.collectionItem}>
-                    
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                                                <li className={styles.collectionItem}>
-                    
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                                                <li className={styles.collectionItem}>
-                    
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                                                <li className={styles.collectionItem}>
-                    
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
-                                                <li className={styles.collectionItem}>
-                    
-                        <img className={styles.collectionImage} src="https://www.fillmurray.com/640/360" alt="" />
-                        <p className={styles.collectionText}>
-                            Романтические комедии
-                        </p>
-                </li>
+                    {collections.map((collection, index) => 
+                        <li key={index} onClick={() => { openModalWithCollection(collection) }} className={styles.collectionItem}>
+                            <Collection collection={collection} />
+                        </li>)}
             </ul>
             </div>
-            <Modal show={show} onClose={() => { setShow(false) }} collections={collections}/>
+            <Modal show={show} onClose={() => { setShow(false) }} collection={currentCollection}/>
         </>
     )
 }
