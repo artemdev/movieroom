63d4af495ff9175d5eae8856cda9b503234dbfda
Artem Zymovets <artwayprojects@gmail.com>
2021-03-20 19:08:17 +0200

    collections

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,15 +1,22 @@
-import styles from './styles.module.css'
-import logo from "../../images/logo.svg";
+import styles from './styles.module.css';
+import logo from '../../images/logo.png';
 
 export default function NavBar(props) {
-    const signIn = true
+    const signIn = true;
     return (
         <header className={styles.navBar}>
             <div className={styles.headerLogo}>
-                <img className={styles.headerImage} src={logo} alt="" />   
+                <img className={styles.headerImage} src={logo} alt="" />
             </div>
-            {signIn ? <div className={styles.headerMenu}>Username <a href="#">выход</a></div> : <div className={styles.headerMenu}><a href="#">создать комнату</a></div>}
-
-        </header >
-    )
+            {signIn ? (
+                <div className={styles.headerMenu}>
+                    Username <a href="#">выход</a>
+                </div>
+            ) : (
+                <div className={styles.headerMenu}>
+                    <a href="#">создать комнату</a>
+                </div>
+            )}
+        </header>
+    );
 }
