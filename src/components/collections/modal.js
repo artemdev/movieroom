b95f78f67801cd24d738bca9951613c3d2d08558
Artem Zymovets <artwayprojects@gmail.com>
2021-03-21 15:53:29 +0200

    modal

--- src/components/collections/modal.js
+++ src/components/collections/modal.js
@@ -1,29 +1,29 @@
-import styles from './styles.module.css'
-import ModalCollection from './modalCollection'
+import styles from './styles.module.css';
+import ModalCollection from './modalCollection';
 export default function Modal({ show, collection, onClose }) {
     if (!show) {
-        return null
+        return null;
     }
-    console.log(collection.movies)
     return (
-
         <div className={styles.modal}>
             <div className={styles.modalContent}>
-                <div className={styles.modalHeader}>
-                    <h2 className={styles.modalTitle}>Романтические комедии</h2>
-                </div>
                 <div className={styles.modalBody}>
-                    {collection.movies && collection.movies.map(movie =>
-                        <div className={styles.modalCollectionBody}>
-                            <ModalCollection movie={movie}/>
-                        </div>
-                    )}
+                    {collection.movies &&
+                        collection.movies.map(movie => (
+                            <div className={styles.modalCollectionBody}>
+                                <ModalCollection movie={movie} />
+                            </div>
+                        ))}
                 </div>
                 <div className={styles.modalFooter}>
-                    <button onClick={onClose} className={styles.modalClose}>Отмена</button>
-                     <button onClick={onClose} className={styles.modalNext}>Выбрать</button>
+                    <button onClick={onClose} className={styles.modalClose}>
+                        Отмена
+                    </button>
+                    <button onClick={onClose} className={styles.modalNext}>
+                        Выбрать
+                    </button>
                 </div>
             </div>
         </div>
-    )
+    );
 }
