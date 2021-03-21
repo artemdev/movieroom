f51a0807fa90c27c52b9ff9886a8e68cc042d02f
Artem Zymovets <artwayprojects@gmail.com>
2021-03-21 15:53:29 +0200

    modal

--- src/components/collections/collection.js
+++ src/components/collections/collection.js
@@ -1,16 +1,17 @@
-
-
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 export default function Collection({ collection }) {
-    console.log(collection)
-    return ( 
-                <>
-                    <img className={styles.collectionImage} src={collection.url} alt="" />
-                    <p className={styles.collectionText}>
-                        <h4>{collection.title}</h4>
-                    </p>
-                </>
-    )
-}  
-
+    console.log(collection);
+    return (
+        <>
+            <img
+                className={styles.collectionImage}
+                src={collection.url}
+                alt=""
+            />
+            <p className={styles.collectionText}>
+                <h4>{collection.title}</h4>
+            </p>
+        </>
+    );
+}
--- src/components/collections/modalCollection.js
+++ src/components/collections/modalCollection.js
@@ -1,21 +1,35 @@
-import styles from './styles.module.css'
+import styles from './styles.module.css';
 
 export default function Collection({ movie }) {
-    return ( 
-                <>
-                <img className={styles.modalCollectionImage} src={movie.coverUrl} alt="" />
-                <p className={styles.modalCollectionText}>
-                    <h4>{movie.title}</h4>
-                <span className={styles.modalCollectionRating}>{movie.rating}</span>
-                <span className={styles.modalCollectionYear}>{movie.year}</span>
-                <span className={styles.modalCollectionGenres}>
-                    {movie.genres && movie.genres.map(genre => genre )}
+    return (
+        <>
+            <img
+                className={styles.modalCollectionImage}
+                src={movie.coverUrl}
+                alt=""
+            />
+            <p className={styles.modalCollectionText}>
+                <h4>{movie.title}</h4>
+                <span className={styles.modalCollectionRating}>
+                    Рейтинг: {movie.rating}
                 </span>
-                <span className={styles.modalCollectionGenres}>{movie.rating}</span>
-                <span className={styles.modalCollectionDuration}>{movie.duration}</span>
-                <span className={styles.modalCollectionDuration}></span>
-                 </p>
-                    </>
-    )
-}  
-
+                <span className={styles.modalCollectionGenres}>
+                    Жанры:{' '}
+                    {movie.genres &&
+                        movie.genres.map((genre, i) => {
+                            if (i !== movie.genres.length - 1) {
+                                genre = `${genre}, `;
+                            }
+                            return genre;
+                        })}
+                </span>
+                <span className={styles.modalCollectionYear}>
+                    Год создания: {movie.year}
+                </span>
+                <span className={styles.modalCollectionDuration}>
+                    Продолжительность: {movie.duration}
+                </span>
+            </p>
+        </>
+    );
+}
