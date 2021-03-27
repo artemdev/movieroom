2785132d0bee9e7f902a64a6541ed15082091928
Artem Zymovets <artwayprojects@gmail.com>
2021-03-27 17:35:24 +0200

    changes

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from './redux/store';
-// import 'index.css';
 
 import 'modern-normalize/modern-normalize.css';
+import './custom.css';
 
 ReactDOM.render(
     <React.StrictMode>
