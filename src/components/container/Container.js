89f2d5b548a19e90b666973ae899d52824e28bc2
Artem Zymovets <artwayprojects@gmail.com>
2021-03-27 17:27:16 +0200

    changes

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -1,5 +1,10 @@
-import styles from './style.module.css';
+import PropTypes from 'prop-types';
+import s from './Container.module.css';
 
 export default function Container({ children }) {
-    return <div className={styles.container}>{children}</div>;
+  return <div className={s.container}>{children}</div>;
 }
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+};
