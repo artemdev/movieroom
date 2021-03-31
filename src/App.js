bb6f8442b8bb04191766b54c753ca71dad42699e
Ilya <ilyalysenko233@gmail.com>
2021-03-31 20:20:40 +0300

    create voteRoom

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import { Suspense } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom'
-import NavBarView from './components/navBar'
-import roomOpenResults from './components/roomResults/roomOpenResults'
-import roomClosedResults from './components/roomResults/roomClosedResults'
-import Container from './components/container'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NavBarView from './components/navBar';
+import roomOpenResults from './components/roomResults/roomOpenResults';
+import roomClosedResults from './components/roomResults/roomClosedResults';
+import Container from './components/container';
 import './App.css';
-import Collections from './components/collections';
+// import Collections from './components/collections';
+import VoteRoom from './components/voteRoom/voteRoom';
 
 function App() {
-  return (
-    <Container>
-      <Router>
-        <Switch>
-          <Suspense fallback='Loading ...'>
-            <NavBarView />
-            <Route exact path='/collections' component={Collections} />
-            <Route exact path='/closed' component={roomClosedResults} />
-            <Route exact path='/open' component={roomOpenResults} />
-          </Suspense>
-        </Switch>
-      </Router>
-    </Container>
-  );
+    return (
+        <Container>
+            <Router>
+                <Switch>
+                    <Suspense fallback="Loading ...">
+                        {/* <NavBarView /> */}
+                        {/* <Route
+                            exact
+                            path="/collections"
+                            component={Collections}
+                        /> */}
+                        <Route
+                            exact
+                            path="/closed"
+                            component={roomClosedResults}
+                        />
+                        <Route exact path="/open" component={roomOpenResults} />
+                        <Route exact path="/voteRoom" component={VoteRoom} />
+                    </Suspense>
+                </Switch>
+            </Router>
+        </Container>
+    );
 }
 
 export default App;
