de5461fac7d488f73677f49bda29122bed196ed0
Artem Zymovets <artwayprojects@gmail.com>
2021-03-31 16:08:48 +0300

    room is creating from API movies

--- src/components/collections/api.js
+++ src/components/collections/api.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 const API_KEY = 'ca745db198ca3fbe8342f07480e09405';
+const API_BACKEND = 'https://movierooms.herokuapp.com/rooms/create';
 
-const fetchCollection = async (id = 10) => {
+export const fetchCollection = async (id = 10) => {
     return await axios
         .get(`https://api.themoviedb.org/3/collection/${id}?api_key=${API_KEY}`)
         .then(({ data }) => data)
         .catch(e => console.log(e));
 };
 
-export default fetchCollection;
+export const createRoom = async movies => {
+    try {
+        await axios.post(API_BACKEND, { movies });
+    } catch (error) {
+        console.log(error);
+    }
+};
