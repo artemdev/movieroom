4c1853b7b62c71fba4347554da6fd8fd6f4083e3
Artem Zymovets <artem.zimovets@gmail.com>
2021-05-23 14:46:12 +0300

    updates

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -2,99 +2,99 @@ import * as API from '../../services/api-auth';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 const register = createAsyncThunk(
-  'auth/register',
-  async (credentials, { rejectWithValue }) => {
-    try {
-      const { data } = await API.register(credentials);
+    'auth/register',
+    async (credentials, { rejectWithValue }) => {
+        try {
+            const { data } = await API.register(credentials);
 
-      API.token.set(data.token);
+            API.token.set(data.token);
 
-      return data;
-    } catch (error) {
-      if (error.response.status === 400) {
-        console.error('Account with this email is already exist!');
-      }
+            return data;
+        } catch (error) {
+            if (error.response.status === 400) {
+                console.error('Account with this email is already exist!');
+            }
 
-      if (error.response.status === 500) {
-        console.error('Server is currently unavailable!');
-      }
+            if (error.response.status === 500) {
+                console.error('Server is currently unavailable!');
+            }
 
-      return rejectWithValue(error.message);
-    }
-  },
+            return rejectWithValue(error.message);
+        }
+    },
 );
 
 const logIn = createAsyncThunk(
-  'auth/login',
-  async (credentials, { rejectWithValue }) => {
-    try {
-      const { data } = await API.logIn(credentials);
+    'auth/login',
+    async (credentials, { rejectWithValue }) => {
+        try {
+            const { data } = await API.logIn(credentials);
 
-      API.token.set(data.token);
+            API.token.set(data.token);
 
-      return data;
-    } catch (error) {
-      if (error.response.status === 400) {
-        console.error('There is no user with this email and password!');
-      }
+            return data;
+        } catch (error) {
+            if (error.response.status === 400) {
+                console.error('There is no user with this email and password!');
+            }
 
-      return rejectWithValue(error.message);
-    }
-  },
+            return rejectWithValue(error.message);
+        }
+    },
 );
 
 const logOut = createAsyncThunk(
-  'auth/logout',
-  async (_, { rejectWithValue }) => {
-    try {
-      await API.logOut();
+    'auth/logout',
+    async (_, { rejectWithValue }) => {
+        try {
+            await API.logOut();
 
-      API.token.unset();
-    } catch (error) {
-      if (error.response.status === 401) {
-        console.error('You are not authorized!');
-      }
+            API.token.unset();
+        } catch (error) {
+            if (error.response.status === 401) {
+                console.error('You are not authorized!');
+            }
 
-      if (error.response.status === 500) {
-        console.error('Server is currently unavailable!');
-      }
+            if (error.response.status === 500) {
+                console.error('Server is currently unavailable!');
+            }
 
-      return rejectWithValue(error.message);
-    }
-  },
+            return rejectWithValue(error.message);
+        }
+    },
 );
 
 const fetchCurrentUser = createAsyncThunk(
-  'auth/refresh',
-  async (_, { rejectWithValue, getState }) => {
-    const state = getState();
+    'auth/refresh',
+    async (_, { rejectWithValue, getState }) => {
+        const state = getState();
 
-    const persistedToken = state.auth.token;
+        const persistedToken = state.auth.token;
 
-    if (persistedToken === null) {
-      return rejectWithValue();
-    }
+        if (persistedToken === null) {
+            return rejectWithValue();
+        }
 
-    API.token.set(persistedToken);
-    try {
-      const { data } = await API.fetchCurrentUser();
+        API.token.set(persistedToken);
+        try {
+            const { data } = await API.fetchCurrentUser();
 
-      return data;
-    } catch (error) {
-      if (error.response.status === 401) {
-        console.error('You are not authorized!');
-      }
+            return data;
+        } catch (error) {
+            if (error.response.status === 401) {
+                console.error('You are not authorized!');
+            }
 
-      return rejectWithValue(error.message);
-    }
-  },
+            return rejectWithValue(error.message);
+        }
+    },
 );
 
 const operations = {
-  register,
-  logOut,
-  logIn,
-  fetchCurrentUser,
+    register,
+    logOut,
+    logIn,
+    fetchCurrentUser,
 };
 
 export default operations;
