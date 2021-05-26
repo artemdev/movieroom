e330014d81421fb5515ddd936cfee9b0957942cb
Artem Zymovets <artem.zimovets@gmail.com>
2021-05-27 01:03:49 +0300

    redirect

--- src/components/collections/modal.js
+++ src/components/collections/modal.js
@@ -1,16 +1,23 @@
 import styles from './styles.module.css';
 import ModalCollection from './modalCollection';
-import { roomsOperations } from '../../redux/rooms/index';
+import { roomsOperations } from '../../redux/rooms';
+import { roomsSelectors } from '../../redux/rooms';
 import { useDispatch } from 'react-redux';
+
+import { useSelector } from 'react-redux';
+
 export default function Modal({ show, onClose, movies }) {
+    // redirect when room is opened
+    const roomOpened = useSelector(roomsSelectors.getIsOpen);
+    roomOpened && (window.location = '/rooms');
+
     const dispatch = useDispatch();
     if (!show) {
         return null;
     }
     const handleSubmit = () => {
         dispatch(roomsOperations.create(movies));
-        //обновить стейт переменной openRoom: true
-        console.log('set open room to true');
+
         onClose();
     };
 
