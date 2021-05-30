f27a47007874d5b7932a52d79f875720989e3594
Artem Zymovets <artem.zimovets@gmail.com>
2021-05-30 22:29:15 +0300

    modal redirect

--- src/components/collections/index.js
+++ src/components/collections/index.js
@@ -16,7 +16,7 @@ export default function Collections(props) {
     const roomOpened = useSelector(roomsSelectors.getIsOpen);
 
     if (roomOpened) {
-        window.location = '/rooms/results';
+        window.location = '/rooms/open';
     }
 
     const openModalWithCollection = (collection = false) => {
