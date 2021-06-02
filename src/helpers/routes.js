d4eaab682470bd0388d118e43a29ec6f0bdaa9ec
Artem Zymovets <artem.zimovets@gmail.com>
2021-06-02 23:01:28 +0300

    updates

--- src/helpers/routes.js
+++ src/helpers/routes.js
@@ -1,4 +1,5 @@
 const API_BACKEND = 'https://movierooms.herokuapp.com';
+// const API_BACKEND = 'localhost:3001';
 const CREATE_ROOM_URL = `${API_BACKEND}/rooms`;
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original/';
 
