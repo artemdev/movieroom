7649e557fbb6824968a22a1f623bb3c1c1436832
Artem Zymovets <artem.zimovets@gmail.com>
2021-06-08 15:06:38 +0300

    Update index.js

--- src/components/navBar/index.js
+++ src/components/navBar/index.js
@@ -1,1 +1,25 @@
-export { default } from './NavBar';
+import styles from './styles.module.css';
+import logo from '../../images/logo.png';
+
+export default function NavBar() {
+    const signIn = true;
+    const signOut = 'Выйти';
+    return (
+        <>
+            <header className={styles.navBar}>
+                <div className={styles.headerLogo}>
+                    <img className={styles.headerImage} src={logo} alt="" />
+                </div>
+                {signIn ? (
+                    <div className={styles.headerMenu}>
+                        <a href="/#">{signOut}</a>
+                    </div>
+                ) : (
+                    <div className={styles.headerMenu}>
+                        <a href="/#">Создать комнату</a>
+                    </div>
+                )}
+            </header>
+        </>
+    );
+}
