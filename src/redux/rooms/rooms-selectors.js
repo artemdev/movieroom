b2c1ee84a4f8a1490213a5f9f8f22f7bc979a9d4
Artem Zymovets <artem.zimovets@gmail.com>
2021-11-06 18:26:14 +0200

    total people voted

--- src/redux/rooms/rooms-selectors.js
+++ src/redux/rooms/rooms-selectors.js
@@ -1,13 +1,15 @@
 const getIsOpen = state => state.rooms.isOpen;
-// const getMovies = state => state.rooms.movies;
 const getCurrentMovie = state => state.rooms.currentMovie;
 const getMovieIsLoading = state => state.rooms.currentMovie.isLoading;
 const getResultsVotes = state => state.rooms.results;
+const getTotalPeopleVoted = state => 2;
+
 const roomsSelectors = {
     getIsOpen,
     getCurrentMovie,
     getMovieIsLoading,
     getResultsVotes,
+    getTotalPeopleVoted,
 };
 
 export default roomsSelectors;
